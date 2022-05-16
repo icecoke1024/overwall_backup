7d5eec2d56063b275910155f2238db2c85243162
as222 <92660153+icecoke1024@users.noreply.github.com>
2022-05-17 02:55:55 +0800

    Update Overwall_Gitee_mirror.sh

--- Overwall_Gitee_mirror.sh
+++ Overwall_Gitee_mirror.sh
@@ -13,22 +13,27 @@ do
 done
 
 # 防火墙规则部分(CDN)
-sed -i 's#https://raw.githubusercontent.com/icecoke1024/overwall_backup/master/KkFCtZkeAP/eFw58nNRXXfTwU4#g' ${root}/update
-sed -i 's#https://github.com/icecoke1024/overwall_backup/blob/master/KkFCtZkeAP/t8eOh94EJIHTXR6#g' ${root}/update
-sed -i 's#https://github.com/icecoke1024/overwall_backup/blob/master/KkFCtZkeAP/avtPeqDKt645Arm#g' ${root}/update
+sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/eFw58nNRXXfTwU4#https://gitee.com/fangxx3863/overwall_backup/raw/master/KkFCtZkeAP/eFw58nNRXXfTwU4#g' ${root}/update
+sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/t8eOh94EJIHTXR6#https://gitee.com/fangxx3863/overwall_backup/raw/master/KkFCtZkeAP/t8eOh94EJIHTXR6#g' ${root}/update
+sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/avtPeqDKt645Arm#https://raw.githubusercontent.com/icecoke1024/overwall_backup/master/KkFCtZkeAP/avtPeqDKt645Arm#g' ${root}/update
+
+sed -i 's#https://cdn.jsdelivr.net/gh/fangxx3863/overwall_list/China_IPList#https://gitee.com/fangxx3863/overwall_backup/raw/master/KkFCtZkeAP/eFw58nNRXXfTwU4#g' ${root}/update
+sed -i 's#https://cdn.jsdelivr.net/gh/fangxx3863/overwall_list/China_IPv6List#https://gitee.com/fangxx3863/overwall_backup/raw/master/KkFCtZkeAP/t8eOh94EJIHTXR6#g' ${root}/update
+sed -i 's#https://cdn.jsdelivr.net/gh/fangxx3863/overwall_list/GFW_List#https://raw.githubusercontent.com/icecoke1024/overwall_backup/master/KkFCtZkeAP/avtPeqDKt645Arm#g' ${root}/update
+
 
 
 # 防火墙规则部分(GITHUB)
-sed -i 's#https://raw.githubusercontent.com/icecoke1024/overwall_backup/master/KkFCtZkeAP/eFw58nNRXXfTwU4#g' ${root}/update
-sed -i 's#https://github.com/icecoke1024/overwall_backup/blob/master/KkFCtZkeAP/t8eOh94EJIHTXR6#g' ${root}/update
-sed -i 's#https://github.com/icecoke1024/overwall_backup/blob/master/KkFCtZkeAP/avtPeqDKt645Arm#g' ${root}/update
+sed -i 's#https://raw.githubusercontent.com/fangxx3863/overwall_list/main/China_IPList#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/eFw58nNRXXfTwU4#g' ${root}/update
+sed -i 's#https://raw.githubusercontent.com/fangxx3863/overwall_list/main/China_IPv6List#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/t8eOh94EJIHTXR6#g' ${root}/update
+sed -i 's#https://raw.githubusercontent.com/fangxx3863/overwall_list/main/GFW_List#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/avtPeqDKt645Arm#g' ${root}/update
 
 
 # 分流域名部分
-sed -i 's#https://github.com/icecoke1024/overwall_backup/raw/master/KkFCtZkeAP#g' ${root}/preload
-sed -i 's#https://github.com/icecoke1024/overwall_backup/raw/master/KkFCtZkeAP#g' ${root}/preload
+sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP#https://gitee.com/fangxx3863/overwall_backup/raw/master/KkFCtZkeAP#g' ${root}/preload
+sed -i 's#https://cdn.jsdelivr.net/gh/fangxx3863/overwall_list/Bypass#https://gitee.com/fangxx3863/overwall_backup/raw/master/KkFCtZkeAP#g' ${root}/preload
 
-sed -i 's#https://github.com/icecoke1024/overwall_backup/tree/master/KkFCtZkeAP#g' ${root}/preload
+sed -i 's#https://github.com/icecoke1024/overwall_backup/tree/master/KkFCtZkeAP#https://github.com/icecoke1024/overwall_backup/tree/master/KkFCtZkeAP#g' ${root}/preload
 # NF
 sed -i 's#NF_IPList_Small#MnwTZoJKZA7F3xf#g' ${root}/preload
 sed -i 's#NF_IPList_Big#S1WMOUf0VmdLPT9#g' ${root}/preload
